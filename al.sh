1f771370b0087d7387e71a5abfd3b168af4832cc
田中賢三 <kenzo@tanakakenzou-no-MacBook-Pro.local>
2015-04-30 16:58:45 +0900

    オートログイン実装

--- al.sh
+++ al.sh
@@ -1,30 +1,25 @@
 #!/bin/sh
 
 SITE_ALIAS=$1
+ENV=$2
 echo "SITE_ALIAS:${SITE_ALIAS}"
-
 CONFIG="projects.yaml"
 
-PROJECT=$(ruby -ryaml -e 'puts YAML.load(open("${CONFIG}").read)["${SITE_ALIAS}"]["project"]')
-echo "PROJECT:${PROJECT}"
+PROJECT=$(ruby -ryaml -e "puts YAML.load(open('${CONFIG}').read)['${SITE_ALIAS}']['project']")
+IP=$(ruby -ryaml -e "puts YAML.load(open('${CONFIG}').read)['${SITE_ALIAS}']['${ENV}']['ip']")
+ID=$(ruby -ryaml -e "puts YAML.load(open('${CONFIG}').read)['${SITE_ALIAS}']['${ENV}']['id']")
+PW=$(ruby -ryaml -e "puts YAML.load(open('${CONFIG}').read)['${SITE_ALIAS}']['${ENV}']['pw']")
+ 
 
-exit
- 
-auto_ssh() {
-host=$1
-id=$2
-pass=$3
- 
 expect -c "
 set timeout 10
-spawn ssh ${id}@${host}
+spawn ssh ${ID}@${IP}
 expect \"Are you sure you want to continue connecting (yes/no)?\" {
     send \"yes\n\"
-    expect \"${id}@${host}'s password:\"
-    send \"${pass}\n\"
-} \"${id}@${host}'s password:\" {
-    send \"${pass}\n\"
+    expect \"${ID}@${IP}'s password:\"
+    send \"${PW}\n\"
+} \"${id}@${IP}'s password:\" {
+    send \"${PW}\n\"
 }
 interact
 "
-}
