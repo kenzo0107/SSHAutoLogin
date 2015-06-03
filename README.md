9e3acad64f604b19dea3ea19e7d1d4e08ad73522
田中賢三 <kenzo.tanaka0107@gmail.com>
2015-06-03 14:55:33 +0900

    追加 : scp 簡易化シェル ファイル

--- README.md
+++ README.md
@@ -1,35 +1,34 @@
 # SSHAutoLogin
-リモートサーバから設定ファイル(projects.yaml)を読み込み、SSHでログインする
- 
- 
+リモートサーバから設定ファイル(yaml)を読み込み、SSHでログインする
+
+
 ## al.sh (Auto Login)
 
 * サイトエイリアスと環境名を指定してsshログインするシェルです。
- 
- 
+
+
 #### 使い方
 
 `sh al.sh <project_alias> <接続環境 (dev|tst|stg|prd)>`
- 
- 
-* ex) Cole Haan のテスト環境に接続
 
-`sh al.sh ch tst`
- 
- 
- 
- 
+
+* ex) mameko のテスト環境に接続
+
+`sh al.sh mameko tst`
+
+
+
+
 ## ac.sh (Auto aCcount)
 
 * サイトエイリアスと環境名を指定してそのサーバ情報や関連情報を表示します。
- 
- 
+
+
 #### 使い方
 
 `sh ac.sh <project_alias> <接続環境 (dev|tst|stg|prd)>`
- 
- 
-* ex) 3.1 Philliplim の商用環境情報表示
 
-`sh ac.sh pl tst`
 
+* ex) mameko の商用環境情報表示
+
+`sh ac.sh mameko tst`
---
+++ as.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Auto Download By SCP
+
+SITE_ALIAS=$1
+ENV=$2
+TARGET_DIR=$3
+BACKUP_DIR=$4
+CONFIG="projects.yaml"
+
+PROJECT=$(ruby -ryaml -e "puts YAML.load(open('${CONFIG}').read)['${SITE_ALIAS}']['project']")
+IP=$(ruby -ryaml -e "puts YAML.load(open('${CONFIG}').read)['${SITE_ALIAS}']['${ENV}']['ip']")
+ID=$(ruby -ryaml -e "puts YAML.load(open('${CONFIG}').read)['${SITE_ALIAS}']['${ENV}']['id']")
+PW=$(ruby -ryaml -e "puts YAML.load(open('${CONFIG}').read)['${SITE_ALIAS}']['${ENV}']['pw']")
+
+echo "\n connect to ${PROJECT} : ${ENV} ...\n" 
+
+echo "\n Download ${TARGET_DIR} to ${BACKUP_DIR} \n"
+
+expect -c "
+set timeout 10
+spawn scp -Cpqr ${ID}@${IP}:${TARGET_DIR} ${BACKUP_DIR}
+expect {
+\" Are you sure you want to continue connecting (yes/no)? \" {
+send \"yse\r\"
+expect \"password:\"
+send \"${PW}\r\"
+} \"password:\" {
+send \"${PW}\r\"
+}
+}
+interact
+"
+
+echo "Finish."
